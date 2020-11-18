10ea8ec1c323d01d9ab2e1bd80a67fe35c991106
Ang98 <angelcayatopa98@gmail.com>
2020-11-17 23:19:42 -0500

    ahora si >:V

--- src/controllers/medica-controller.js
+++ src/controllers/medica-controller.js
@@ -2,18 +2,31 @@ const FichaMedica = require("../model/FichaMedica");
 const User = require("../model/User");
 
 const createFichaMedica = async (req, res) => {
+  const {
+    diagnostico,
+    tipoSangre,
+    medicamentoHabitual,
+    medicamentosAlergicos,
+    seguroMedico,
+    anio,
+    email,
+  } = req.body;
 
-    const { diagnostico,tipoSangre,medicamentoHabitual,medicamentosAlergicos,seguroMedico,email } = req.body;
+  const newFM = new FichaMedica({
+    diagnostico,
+    tipoSangre,
+    medicamentoHabitual,
+    medicamentosAlergicos,
+    seguroMedico,
+    anio
+  });
 
-    
-    usuario.fichaMedica.add(fm._id)
-    usuario.save()
+  const fm = await newFM.save()
+  const usuario = await User.findOne({email})
+  usuario.fichaMedica.push(fm._id)
+  const savedUsuario = await usuario.save()
+  
+  res.status(200).json({ error: false, message:'La ficha medica fue creada correctamente',savedUsuario})
+};
 
-    fm = new FichaMedica()
-
-    ususario=User.find({email}),
-    usuario.fichaMedica.push(fm._id)
-    usuario
-}
-
-module.exports = { createFichaMedica }
+module.exports = { createFichaMedica };
