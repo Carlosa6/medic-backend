ebff002e0a5090721a88b9219ba9bb24e2110af3
Carlosa6 <k5r6ri89yo@gmail.com>
2020-11-18 19:07:50 -0500

    probando nueva configuracion cors

--- src/index.js
+++ src/index.js
@@ -13,15 +13,20 @@ const tipoSangreRouter = require('./routes/tipo-sangre-routes')
 require('./database')
 
 const port = process.env.PORT || 4000
+const corOptions = {
+    credentials: true,
+    origin: true,
+    optionsSuccessStatus: 200
+}
 
 app.use(morgan('dev'))
-app.use(cors({credentials: true, origin: true}))
+app.use(cors(corOptions))
 app.use(express.json())
 
 //rutas
-app.use('/api/users', userRoutes)
-app.use('/api/auth', authRoutes)
-app.use('/api/medic', fichaMedicaRouter)
-app.use('/api/tipo-sangre', tipoSangreRouter)
+app.use('/api/users', cors(corOptions), userRoutes)
+app.use('/api/auth', cors(corOptions), authRoutes)
+app.use('/api/medic', cors(corOptions), fichaMedicaRouter)
+app.use('/api/tipo-sangre', cors(corOptions), tipoSangreRouter)
 
 app.listen(port, () => console.log('Server on port', port))
