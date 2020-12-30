dfc297398882fbe72546fe2971d5dd665ae48bef
Carlosa6 <k5r6ri89yo@gmail.com>
2020-12-29 19:58:44 -0500

    Estadisticas tipo de sangre

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,47 +1,74 @@
-const generals = require('../libs/generals')
-const FichaMedica = require('../model/FichaMedica')
+const FichaMedica = require('../model/FichaMedica');
+const TipoSangre = require('../model/TipoSangre');
 
-exports.chartTipoSangre = (req,res) => {
+async function cantidadUsuariosxTipoSangre(tipo, anio) {
+    const cantidad = await FichaMedica.find({ tipoSangre: tipo, anio: anio }).countDocuments()
+    return cantidad;
+}
 
-    const label1 = ["A+","A-","B+","B-","AB+","AB-","O+","O-"]
-    const data1 = [23,45,18,56,28,39,100,62]
+async function idDelTipoDeSangre(tipo) {
+    const elid = await TipoSangre.findOne({ tipo: tipo })
+    return elid._id
+}
 
-    const label2 = generals.SEGURO_MEDICO
-    const data2 = [63,90,2,34,45,13,18,8]
+exports.chartTipoSangre = (req, res) => {
 
-    res.render('chart',{
-        label1:JSON.stringify(label1),
-        data1:JSON.stringify(data1),
-        label2: JSON.stringify(label2),
-        data2: JSON.stringify(data2)
-    })
+    // const labels = ["A+", "A-", "O+"]
+
+    // let amas = {
+    //     a2017: cantidadUsuariosxTipoSangre(idDelTipoDeSangre("Amas"), 2017),
+    //     a2018: cantidadUsuariosxTipoSangre(idDelTipoDeSangre("Amas"), 2018),
+    //     a2019: cantidadUsuariosxTipoSangre(idDelTipoDeSangre("Amas"), 2019),
+    //     a2020: cantidadUsuariosxTipoSangre(idDelTipoDeSangre("Amas"), 2020)
+    // }
+
+    // let amenos = {
+    //     a2017: cantidadUsuariosxTipoSangre(idDelTipoDeSangre("Amenos"), 2017),
+    //     a2018: cantidadUsuariosxTipoSangre(idDelTipoDeSangre("Amenos"), 2018),
+    //     a2019: cantidadUsuariosxTipoSangre(idDelTipoDeSangre("Amenos"), 2019),
+    //     a2020: cantidadUsuariosxTipoSangre(idDelTipoDeSangre("Amenos"), 2020),
+    // }
+
+    // let omas = {
+    //     o2017: cantidadUsuariosxTipoSangre(idDelTipoDeSangre("Omas"), 2017),
+    //     o2018: cantidadUsuariosxTipoSangre(idDelTipoDeSangre("Omas"), 2018),
+    //     o2019: cantidadUsuariosxTipoSangre(idDelTipoDeSangre("Omas"), 2019),
+    //     o2020: cantidadUsuariosxTipoSangre(idDelTipoDeSangre("Omas"), 2020),
+    // }
+
+    // res.render('chart', {
+    //     labels: JSON.stringify(labels),
+    //     amas: JSON.stringify([amas.a2017, amas.a2018, amas.a2019, amas.a2020]),
+    //     amenos: JSON.stringify([amenos.a2017, amenos.a2018, amenos.a2019, amenos.a2020]),
+    //     omas: JSON.stringify([omas.o2017, omas.o2018, omas.o2019, omas.o2020])
+    // })
 
 }
 
 // FUNCIÓN QUE RETORNA LA CANTDAD DE USUARIOS POR TIPO DE SEGURO MÉDICO UNMSM Y AÑO
-async function countUNMSMByYear(anio){
-    const cantidad = await FichaMedica.find({"seguroMedico.UNMSM":true,anio:anio}).countDocuments()
+async function countUNMSMByYear(anio) {
+    const cantidad = await FichaMedica.find({ "seguroMedico.UNMSM": true, anio: anio }).countDocuments()
     return cantidad
 }
 
 // FUNCIÓN QUE RETORNA LA CANTDAD DE USUARIOS POR TIPO DE SEGURO MÉDICO MINSA Y AÑO
-async function countMINSAyYear(anio){
-    const cantidad = await FichaMedica.find({"seguroMedico.MINSA":true,anio:anio}).countDocuments()
+async function countMINSAyYear(anio) {
+    const cantidad = await FichaMedica.find({ "seguroMedico.MINSA": true, anio: anio }).countDocuments()
     return cantidad
 }
 
 // FUNCIÓN QUE RETORNA LA CANTDAD DE USUARIOS POR TIPO DE SEGURO MÉDICO ESSALUD Y AÑO
-async function countESSALUDByYear(anio){
-    const cantidad = await FichaMedica.find({"seguroMedico.ESSALUD":true,anio:anio}).countDocuments()
+async function countESSALUDByYear(anio) {
+    const cantidad = await FichaMedica.find({ "seguroMedico.ESSALUD": true, anio: anio }).countDocuments()
     return cantidad
 }
 
-exports.seguroMedico = async (req,res) => {
+exports.seguroMedico = async (req, res) => {
     let unmsm = {
-        unmsm2017:await countUNMSMByYear(2017),
-        unmsm2018:await countUNMSMByYear(2018),
-        unmsm2019:await countUNMSMByYear(2019),
-        unmsm2020:await countUNMSMByYear(2020)
+        unmsm2017: await countUNMSMByYear(2017),
+        unmsm2018: await countUNMSMByYear(2018),
+        unmsm2019: await countUNMSMByYear(2019),
+        unmsm2020: await countUNMSMByYear(2020)
     }
     let minsa = {
         minsa2017: await countMINSAyYear(2017),
@@ -55,13 +82,13 @@ exports.seguroMedico = async (req,res) => {
         essalud2019: await countESSALUDByYear(2019),
         essalud2020: await countESSALUDByYear(2020)
     }
-    const seguros = ["UNMSM","MINSA","ESSALUD"]
-    const anios = ["2017","2018","2019","2020"]
-    res.render('seguroMedico',{
+    const seguros = ["UNMSM", "MINSA", "ESSALUD"]
+    const anios = ["2017", "2018", "2019", "2020"]
+    res.render('seguroMedico', {
         seguros: JSON.stringify(seguros),
         anios: JSON.stringify(anios),
-        unmsm:JSON.stringify([unmsm.unmsm2017,unmsm.unmsm2018,unmsm.unmsm2019,unmsm.unmsm2020]),
-        minsa:JSON.stringify([minsa.minsa2017,minsa.minsa2018,minsa.minsa2019,minsa.minsa2020]),
-        essalud:JSON.stringify([essalud.essalud2017,essalud.essalud2018,essalud.essalud2019,essalud.essalud2020]),
+        unmsm: JSON.stringify([unmsm.unmsm2017, unmsm.unmsm2018, unmsm.unmsm2019, unmsm.unmsm2020]),
+        minsa: JSON.stringify([minsa.minsa2017, minsa.minsa2018, minsa.minsa2019, minsa.minsa2020]),
+        essalud: JSON.stringify([essalud.essalud2017, essalud.essalud2018, essalud.essalud2019, essalud.essalud2020]),
     })
 }
