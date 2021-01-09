4b818b61289cdc9660da827c406da5a5e6ce7dc7
Carlosa6 <k5r6ri89yo@gmail.com>
2021-01-09 17:52:33 -0500

    Verificar lo que envia en la consola

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -14,6 +14,11 @@ async function tipoSangrexAnio(anio) {
                 _id: '$tipoSangre',
                 count: { $sum: 1 }
             }
+        },
+        {
+            $sort:{
+                count:-1
+            }
         }
     ])
     return tiposangre
@@ -44,30 +49,56 @@ exports.charts = async (req, res) => {
     //cantidad de fichas médicas por año
     let cantidadFichasxAnio = await FichaMedica.aggregate([
         {
-            $group: {
+            $group: { //Agrupar por año
                 _id: '$anio',
                 count: { $sum: 1 }
             }
+        },
+        {
+            $sort: { //Los años obtenidos ordenarlos descendientemente
+                _id: -1
+            }
+        },
+        { //Devolver sólo los últimos 5 años
+            $limit: 5
         }
     ])
-    arrayEnviar.push({cantidadFichasxAnio:cantidadFichasxAnio})
+    arrayEnviar.push({ cantidadFichasxAnio: cantidadFichasxAnio })
 
+    //TIPOS DE SANGRE
     let tiposDeSangre = await TipoSangre.find({}, { tipo: 0 })
     //Devuelve: [{_id:'dsd3fdfdf',representation:'A+'},{...},...] 8 documentos
-    let tiposanio = await tipoSangrexAnio(2017)
-
+    let tiposanio = await tipoSangrexAnio(2020)
+    
+        // [
+        // { _id: 5fac9d0d3b441334a01a6295, count: 1 },
+        // { _id: 5fac9d0d3b441334a01a6293, count: 2 }
+        // ]
+    
     let representacion = []
 
-    for(let tiposSng in tiposDeSangre){
+    for (let tiposSng in tiposDeSangre) {
         let currentId = tiposDeSangre[tiposSng]._id
-        if(tiposanio.some(obj => JSON.stringify(obj._id) === JSON.stringify(currentId))){
-            representacion.push(tiposDeSangre[tiposSng].representation)
+        if (tiposanio.some(obj => JSON.stringify(obj._id) === JSON.stringify(currentId))) {
+            representacion.push({
+                represt:tiposDeSangre[tiposSng].representation
+            })
         }
     }
-    // console.log(representacion)
-    // console.log(tiposanio)
     
-    res.status(200).json(arrayEnviar)
+    tiposanio.forEach(g => {
+        let cant = g.count
+        representacion.some(obj => {
+            obj.cantidad = cant
+        })
+    })
+
+    console.log(tiposanio)
+    console.log(representacion)
+    // console.log(tiposanio)
+    // console.log(cantidadFichasxAnio)
+
+    // res.status(200).json(arrayEnviar)
 
 }
 
