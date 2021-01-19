28deb0611b5cbb76bb3986e3a246ed58440128e0
Carlosa6 <k5r6ri89yo@gmail.com>
2021-01-19 16:53:37 -0500

    adicion validacion admin y token ruta busqueda

--- src/routes/busqueda-user-routes.js
+++ src/routes/busqueda-user-routes.js
@@ -3,11 +3,15 @@ const auth = require('../helpers/autenticacion')
 const Busqueda = require('../model/Busqueda')
 
 
-router.get('/', async (req,res) => {
+router.get('/',[
+    auth.verificarToken,auth.isAdmin
+], async (req,res) => {
     const usuariosReales = await Busqueda.find()
     res.status(200).json(usuariosReales)
 })
-router.get('/:dni', async (req,res) => {
+router.get('/:dni',[
+    auth.verificarToken,auth.isAdmin
+], async (req,res) => {
     const userByDni = await Busqueda.findOne({dni:req.params.dni})
 
     if(!userByDni) return res.status(400).json({message:"El Usuario no existe"})
