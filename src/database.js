165a824233c0e491e57084a43583302e8fb9539f
Carlosa6 <k5r6ri89yo@gmail.com>
2022-06-09 15:39:57 -0500

    cambios en credenciales DB

--- src/database.js
+++ src/database.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 
 const MONGODB_URI = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASS}@bwl9ydlhmxufsmx-mongodb.services.clever-cloud.com:27017/${process.env.DB_NAME}`
 
+
+
 mongoose.connect(MONGODB_URI,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
